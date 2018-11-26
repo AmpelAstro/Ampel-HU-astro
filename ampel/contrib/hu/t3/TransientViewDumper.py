02c3f1015692d51575db66133738027d817bfc40
Jakob van Santen <jvansanten@gmail.com>
2018-11-26 21:10:11 +0100

    TransientViewDumper: generate public links on the fly
    NextCloud has an Open Collaboration Services API that exposes the
    same functionality as the little "share link" checkbox in the web
    interface. Neat.

--- ampel/contrib/hu/t3/TransientViewDumper.py
+++ ampel/contrib/hu/t3/TransientViewDumper.py
@@ -13,8 +13,23 @@ from ampel.utils.json import AmpelEncoder, object_hook
 import json
 import requests
 import uuid
-from urllib.parse import urlencode
+from urllib.parse import urlencode, urlparse, urlunparse, ParseResult
+from xml.etree import ElementTree
 from io import StringIO
+
+def strip_auth_from_url(url):
+	try:
+		auth = requests.utils.get_auth_from_url(url)
+		scheme, netloc, path, params, query, fragment = urlparse(url)
+		netloc = netloc[netloc.index('@')+1:]
+		url = urlunparse(ParseResult(scheme, netloc, path, params, query, fragment))
+		return url, auth
+	except KeyError:
+		return url, None
+
+def strip_path_from_url(url):
+	scheme, netloc, path, params, query, fragment = urlparse(url)
+	return urlunparse(ParseResult(scheme, netloc, '/', None, None, None))
 
 class TransientViewDumper(AbsT3Unit):
 	"""
@@ -29,9 +44,12 @@ class TransientViewDumper(AbsT3Unit):
 		self.logger = logger
 		self.count = 0
 		self.outfile = StringIO()
-		self.filename = str(uuid.uuid1()) + '.json'
-		self.dest = base_config['desycloud.default'] + '/AMPEL/' + self.filename
-		self.public = "https://desycloud.desy.de/index.php/s/W6J4sm7H6aAk7aq/download?{}".format(urlencode({'files': self.filename}))
+		self.path = '/AMPEL/dumps/' + str(uuid.uuid1()) + '.json'
+		url, auth = strip_auth_from_url(base_config['desycloud.default'])
+		self.session = requests.Session()
+		self.auth = auth
+		self.webdav_base = url
+		self.ocs_base = strip_path_from_url(url) + '/ocs/v1.php/apps/files_sharing/api/v1'
 		# don't bother preserving immutable types
 		self.encoder = AmpelEncoder(lossy=True)
 
@@ -54,5 +72,14 @@ class TransientViewDumper(AbsT3Unit):
 		mb = len(self.outfile.getvalue().encode()) / 2.0 ** 20
 		self.logger.info("{:.1f} MB of JSONy goodness".format(mb))
 		self.logger.info("Total number of transient printed: %i" % self.count)
-		requests.put(self.dest, data=self.outfile.getvalue()).raise_for_status()
-		self.logger.info(self.public)
+		self.session.put(self.webdav_base + self.path, data=self.outfile.getvalue(), auth=self.auth).raise_for_status()
+		response = self.session.post(self.ocs_base + '/shares',
+		    data=dict(path=self.path, shareType=3),
+		    auth=self.auth, 
+		    headers={'OCS-APIRequest': 'true'} # i'm not a CSRF attack, i swear
+		)
+		if response.ok:
+			public = ElementTree.fromstring(response.text).find('data/url').text
+			self.logger.info(public)
+		else:
+			response.raise_for_status()
