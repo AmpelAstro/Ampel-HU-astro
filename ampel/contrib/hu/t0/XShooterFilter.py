debd07818aa5c525684029b39485623f25f8ad13
vbrinnel <valery.brinnel@gmail.com>
2019-10-31 23:06:54 +0100

    XShooterFilter: v0.7 compatibility update

--- ampel/contrib/hu/t0/XShooterFilter.py
+++ ampel/contrib/hu/t0/XShooterFilter.py
@@ -9,12 +9,11 @@
 
 
 import logging
-from numpy import array, logical_and
-from urllib.parse import urlparse
+from numpy import array
 from astropy.time import Time
-
+from typing import Any, Dict, Optional, Set
+from ampel.base.AmpelAlert import AmpelAlert
 from ampel.contrib.hu.t0.DecentFilter import DecentFilter
-
 
 
 class XShooterFilter(DecentFilter):
@@ -29,41 +28,41 @@ class XShooterFilter(DecentFilter):
 	version = 1.0
 	resources = ('catsHTM.default',)
 	
-	class RunConfig(DecentFilter.RunConfig):
+	class InitConfig(DecentFilter.InitConfig):
 		MAX_DEC			: float	# maximum allowed value for the declination
 		DET_WITHIN		: float	# the transient must have been detected within the last 'DET_WITHIN' days
 		UL_WITHIN		: float # the transient must AT LEAST one ulim within the last 'UL_WITHIN' days
 	
-	def __init__(self, on_match_t2_units, base_config=None, run_config=None, logger=None):
+	def __init__(
+		self, logger: logging.Logger, init_config: Dict[str, Any] = None, 
+		resources: Dict[str, Any] = None
+	):
+
 		"""
 		"""
-		if run_config is None:
+		if init_config is None:
 			raise ValueError("Please check you run configuration")
 
-		self.on_match_t2_units = on_match_t2_units
 		self.logger = logger if logger is not None else logging.getLogger()
 		
 		# init the parent DecentFilter
-		DecentFilter.__init__(self, self.on_match_t2_units, base_config, run_config, logger=self.logger)
-		
-		# parse the run config
-		rc_dict = run_config.dict()
-		for k, val in rc_dict.items():
-			self.logger.info("Using %s=%s" % (k, val))
+		DecentFilter.__init__(self, self.logger, init_config, resources)
 		
 		# remember the pars
-		self.max_dec 					= rc_dict['MAX_DEC']
-		self.det_within					= rc_dict['DET_WITHIN']
-		self.ul_within					= rc_dict['UL_WITHIN']
+		self.max_dec 					= init_config.MAX_DEC
+		self.det_within					= init_config.DET_WITHIN
+		self.ul_within					= init_config.UL_WITHIN
 		self.keys_to_check += ('jd',)
 
-	def apply(self, alert):
+
+	# Override
+	def apply(self, ampel_alert: AmpelAlert) -> Optional[Set[str]]:
 		"""
 		run the decent filter on the alert
 		"""
 		
 		# cut on declination
-		latest = alert.pps[0]
+		latest = ampel_alert.pps[0]
 		if latest['dec'] > self.max_dec:
 			self.logger.debug("rejected: declination %.2f deg is above maximum allowed of %.2f deg"% 
 				(latest['dec'], self.max_dec))
@@ -77,7 +76,7 @@ class XShooterFilter(DecentFilter):
 		self.logger.debug("Setting 'now' to JD %.4f to cut on alert history"%now_jd)
 		
 		# check on history 1: detected in the last 6h
-		detection_jds = array(alert.get_values('jd', upper_limits=False))
+		detection_jds = array(ampel_alert.get_values('jd', upper_limits=False))
 		recent_detections = detection_jds > (now_jd - self.det_within)
 		if not any(recent_detections):
 			self.logger.debug("rejected: no detection within the last %.3f days (latest one %.3f days ago)"%
@@ -85,7 +84,7 @@ class XShooterFilter(DecentFilter):
 			return None
 		
 		# check on the history 2: at least one upper limit in the last 5 days
-		ulim_jds = alert.get_values('jd', upper_limits=True)
+		ulim_jds = ampel_alert.get_values('jd', upper_limits=True)
 		if ulim_jds is None:
 			self.logger.debug("rejected: this alert has no upper limits")
 			return None
@@ -103,4 +102,4 @@ class XShooterFilter(DecentFilter):
 			return None
 		
 		# now apply the DecentFilter
-		return DecentFilter.apply(self, alert)
+		return super().apply(self, ampel_alert)
