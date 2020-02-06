e5992e00ac7792db4b849743697b452985aaf2d2
vbrinnel <valery.brinnel@gmail.com>
2020-02-06 11:19:28 +0100

    ChannelSummaryPublisher: v0.7 compatibility update

--- ampel/contrib/hu/t3/ChannelSummaryPublisher.py
+++ ampel/contrib/hu/t3/ChannelSummaryPublisher.py
@@ -1,65 +1,61 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/contrib/hu/t3/ChannelSummaryPublisher.py
+# File              : Ampel-contrib-HU/ampel/contrib/hu/t3/ChannelSummaryPublisher.py
 # License           : BSD-3-Clause
 # Author            : m. giomi <matteo.giomi@desy.de>
 # Date              : 13.11.2018
-# Last Modified Date: 13.11.2018
-# Last Modified By  : m. giomi <matteo.giomi@desy.de>
+# Last Modified Date: 06.02.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-import backoff
-import json
-import requests
-import uuid
-import time, datetime
+import backoff, requests, datetime
+
+from typing import List
 from pytz import timezone
 from astropy.time import Time
-from urllib.parse import urlencode
 from io import StringIO, BytesIO
-from pydantic import BaseModel
-from typing import List
+from typing import Optional, Dict, Any
 
 from ampel.abstract.AbsT3Unit import AbsT3Unit
-from ampel.utils.json import AmpelEncoder
-from ampel.logging.AmpelLogger import AmpelLogger
+from ampel.utils.json import AmpelEncoder, load
+
 
 class ChannelSummaryPublisher(AbsT3Unit):
 	"""
-		Create a json file with summary statistics for the channel. For the transients
-		detected in the last N days, this json file contains, i.e. coords, RB score,
-		first detection, latest detection, and the total number of transients detected
-		by the channel.
+	Create a json file with summary statistics for the channel. For the transients
+	detected in the last N days, this json file contains, i.e. coords, RB score,
+	first detection, latest detection, and the total number of transients detected
+	by the channel.
 	"""
 
-	version = 0.1
-	resources = ('desycloud.default',)
+	require = ('desycloud.default', )
 
-	class RunConfig(BaseModel):
-		dryRun: bool = False
-		baseDir: str = '/AMPEL/ZTF'
-		alertMetrics: List[str] = ['ztf_name', 'ra', 'dec', 'rb', 'detections', 'first_detection', 'last_detection']
+	dryRun: bool = False
+	baseDir: str = '/AMPEL/ZTF'
+	alertMetrics: List[str] = [
+		'ztf_name', 'ra', 'dec', 'rb', 'detections',
+		'first_detection', 'last_detection'
+	]
 
-	def __init__(self, logger, base_config=None, run_config=None, global_info=None):
-		"""
-		"""
-		self.logger = AmpelLogger.get_logger() if logger is None else logger
+
+	def post_init(self, context: Optional[Dict[str, Any]]) -> None:
+
 		self.summary = {}
 		self._jd_range = [float('inf'), -float('inf')]
-		
-		self.run_config = run_config if run_config is not None else self.RunConfig()
-		self.dest = base_config['desycloud.default'] + self.run_config.baseDir
-		self.logger.info("Channel summary will include following metrics: %s"%repr(self.run_config.alertMetrics))
+		self.dest = self.resource['desycloud.default'] + self.run_config.baseDir
+		self.logger.info(
+			f"Channel summary will include following metrics: {repr(self.alertMetrics)}"
+		)
 		self._channels = set()
 		self.session = requests.Session()
 
+
 	def extract_from_transient_view(self, tran_view):
 		"""
-			given transient view object return a dictionary 
-			with the desired metrics
+		given transient view object return a dictionary
+		with the desired metrics
 		"""
 		metrics = set(self.run_config.alertMetrics)
 		out = {}
-		
 		out['ztf_name'] = tran_view.tran_names[0]
 		out['tns_names'] = tran_view.tran_names[1:]
 
@@ -73,7 +69,7 @@ class ChannelSummaryPublisher(AbsT3Unit):
 		for key in metrics:
 			if key in ['ra', 'dec', 'rb']:
 				out[key] = pps[-1].get(key)
-		
+
 		# look at full det history for start and end of detection
 		if 'first_detection' in metrics:
 			out['first_detection'] = pps[0]['jd']
@@ -91,7 +87,7 @@ class ChannelSummaryPublisher(AbsT3Unit):
 
 	def add(self, transients):
 		"""
-			load the stats from the alerts
+		load the stats from the alerts
 		"""
 		if transients is not None:
 			for tran_view in transients:
@@ -104,9 +100,10 @@ class ChannelSummaryPublisher(AbsT3Unit):
 				self.summary[key] = info_dict
 				self._channels.add(tran_view.channel)
 
-	@backoff.on_exception(backoff.expo,
-	    (TimeoutError, requests.exceptions.HTTPError),
-	    giveup=lambda exc: isinstance(exc,requests.exceptions.HTTPError) and exc.response.status_code not in {400, 403, 405, 423, 500}
+
+	@backoff.on_exception(backoff.expo, (TimeoutError, requests.exceptions.HTTPError),
+		giveup=lambda exc: isinstance(exc, requests.exceptions.HTTPError) and \
+		exc.response.status_code not in {400, 403, 405, 423, 500}
 	)
 	def done(self):
 		"""
@@ -114,7 +111,7 @@ class ChannelSummaryPublisher(AbsT3Unit):
 		if len(self._channels) == 0:
 			return
 		elif len(self._channels) > 1:
-			raise ValueError("Got multiple channels ({}) in summary".format(list(self._channels)))
+			raise ValueError(f"Got multiple channels ({list(self._channels)}) in summary")
 
 		# Find the date of the most recent observation, in Pacific time
 		timestamp = Time(self._jd_range[-1], format='jd').to_datetime(timezone('US/Pacific'))
