ee97194fc767a6e2529695a7458887bcf73c8b99
vbrinnel <valery.brinnel@gmail.com>
2020-03-04 12:44:42 +0100

    setup.py: include yaml files

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-contrib-HU/setup.py
+# File              : setup.py
 # License           : BSD-3-Clause
 # Author            : jvs
 # Date              : Unspecified
-# Last Modified Date: 28.01.2020
+# Last Modified Date: 06.02.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from setuptools import setup, find_namespace_packages
@@ -15,19 +15,23 @@ setup(
 	packages=find_namespace_packages(),
 	package_data = {
 		'': ['*.json'], # include any package containing *.json files
-		'conf': ['*.conf', '**/*.conf', '**/**/*.conf']
+		'conf': [
+			'*.json', '**/*.json', '**/**/*.json',
+			'*.yaml', '**/*.yaml', '**/**/*.yaml',
+			'*.yml', '**/*.yml', '**/**/*.yml'
+		]
 	},
 	entry_points = {
-		'ampel_target_sources' : [
+		'ampel_target_sources': [
 			'TargetSourceListener = ampel.contrib.hu.TargetSourceListener:TargetSourceListener',
 			'TargetSourceListenerSlack = ampel.contrib.hu.TargetSourceListenerSlack:TargetSourceListenerSlack',
 		],
-		'ampel_resources' : [
+		'ampel_resources': [
 			'extcats = ampel.contrib.hu.resources:extcatsURI',
 			'catsHTM = ampel.contrib.hu.resources:catsHTMURI',
 			'desycloud = ampel.contrib.hu.resources:desyCloudURI',
 		],
-		'console_scripts' : [
+		'console_scripts': [
 			'catshtmd = ampel.contrib.hu.catshtm_server:run'
 		]
 	}
