bb7ecc25e410f5dfd077e0742834d6fd06f08bb5
vbrinnel <valery.brinnel@gmail.com>
2020-06-13 23:04:49 +0200

    T2CatalogMatch: v0.7 compatibility update

--- ampel/contrib/hu/t2/T2CatalogMatch.py
+++ ampel/contrib/hu/t2/T2CatalogMatch.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/contrib/hu/t2/T2CatalogMatch.py
+# File              : Ampel-contrib-HU/ampel/contrib/hu/t2/T2CatalogMatch.py
 # License           : BSD-3-Clause
 # Author            : matteo.giomi@desy.de
 # Date              : 24.08.2018
-# Last Modified Date: 24.08.2018
-# Last Modified By  : matteo.giomi@desy.de
+# Last Modified Date: 13.06.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import backoff
+from typing import Sequence, Dict, Any, Optional, Literal
 from pymongo import MongoClient
 from pymongo.errors import AutoReconnect
 from astropy.table import Table
@@ -16,27 +17,38 @@ from zerorpc.exceptions import LostRemote
 from extcats import CatalogQuery
 from extcats.catquery_utils import get_closest
 from numpy import asarray, degrees
-from typing import Dict, Any
 
-from ampel.abstract.AbsT2Unit import AbsT2Unit
-from ampel.flags.T2RunState import T2RunState
+from ampel.abstract.AbsPointT2Unit import AbsPointT2Unit
+from ampel.model.AmpelStrictModel import AmpelStrictModel
+from ampel.t2.T2RunState import T2RunState
 from ampel.contrib.hu.utils import info_as_debug
 from ampel.contrib.hu import catshtm_server
-from ampel.view.LightCurve import LightCurve
+from ampel.content.DataPoint import DataPoint
+from ampel.type import T2UnitResult
 
 
-class T2CatalogMatch(AbsT2Unit):
+class CatalogModel(AmpelStrictModel):
+	use: Literal['extcats', 'catsHTM']
+	rs_arcsec: float
+	catq_kwargs: Dict[str, Any]
+	keys_to_append: Optional[Sequence[str]]
+	pre_filter: Optional[Dict[str, Any]]
+	post_filter: Optional[Dict[str, Any]]
+
+
+class T2CatalogMatch(AbsPointT2Unit):
 	"""
-		cross match the position of a transient to those of sources in a set
-		of catalogs and attach the required information to the transient.
+	Cross matches the position of a transient to those of sources in a set of catalogs
 	"""
 
-	resources = ('extcats.reader', 'catsHTM.default')
+	resources = 'extcats.reader', 'catsHTM.default'
+	catalogs: Dict[str, CatalogModel]
 
 	def post_init(self):
 
 		# empty dict of suppoerted (AS WELL AS REQUESTED) extcats catalog query objects
 		self.catq_objects = {}
+		self.debug = self.logger.verbose > 1
 
 		# initialize the catsHTM paths and the extcats query client.
 		self.catshtm_client = catshtm_server.get_client(self.resource['catsHTM.default'])
@@ -46,12 +58,6 @@ class T2CatalogMatch(AbsT2Unit):
 			'dbclient': self.catq_client,
 			'ra_key': 'ra', 'dec_key': 'dec'
 		}
-
-		# default parameters for LightCurve.get_pos method
-		self.lc_get_pos_defaults = {'ret': "brightest", 'filters': None}
-
-		# mandatory keys
-		self.mandatory_keys = ['use', 'rs_arcsec']
 
 		# decorate run() so that it retries with exponential backoff on
 		# failed reconnects
@@ -96,7 +102,7 @@ class T2CatalogMatch(AbsT2Unit):
 		return catq
 
 
-	def run(self, light_curve: LightCurve, run_config: Dict):
+	def run(self, datapoint: DataPoint) -> T2UnitResult:
 		"""
 		run_parameters: `dict`
 		configuration parameter for this job. There is provision to
@@ -130,36 +136,32 @@ class T2CatalogMatch(AbsT2Unit):
 			the 'catq_kwargs' SHOULD contain the the names of the ra and dec
 			keys in the catalog if those are different from 'ra' and 'dec'
 
-		the 'keys_to_append' parameters is OPTIONAL and specifies wich fileds from
+		the 'keys_to_append' parameters is OPTIONAL and specifies wich fields from
 		the catalog should be returned in case of positional match:
 
-		if not present or if equal to 'all':
+		if not present:
 			all the fields in the given catalog will be returned.
 		if `list`
 			just take this subset of fields.
 
 		Eg:
-
-		run_config = {
-			'get_lc_pos_kwargs': None, # optional see ampel.view.LightCurve doc
-			'catalogs': {
-				'SDSS_spec': {
-					'use': 'extcats',
-					'catq_kwargs': {
-						'ra_key': 'ra',
-						'dec_key': 'dec'
-					},
-					'rs_arcsec': 3,
-					'keys_to_append': ['z', 'bptclass', 'subclass']
+		'catalogs': {
+			'SDSS_spec': {
+				'use': 'extcats',
+				'catq_kwargs': {
+					'ra_key': 'ra',
+					'dec_key': 'dec'
 				},
-				'NED': {
-					'use': 'catshtm',
-					'rs_arcsec': 20,
-					'keys_to_append': ['fuffa1', 'fuffa2', ..],
-					'catq_kwargs':
-				},
-				...
-			}
+				'rs_arcsec': 3,
+				'keys_to_append': ['z', 'bptclass', 'subclass']
+			},
+			'NED': {
+				'use': 'catsHTM',
+				'rs_arcsec': 20,
+				'keys_to_append': ['fuffa1', 'fuffa2', ..],
+				'catq_kwargs':
+			},
+			...
 		}
 
 		:returns: dict with the keys to append to each transient. For example, with the
@@ -180,61 +182,46 @@ class T2CatalogMatch(AbsT2Unit):
 		to the catalog counterpart is also returned as the 'dist2transient' key.
 		"""
 
-		# get ra and dec from lightcurve object
-		lc_get_pos_kwargs = run_config.get('lc_get_pos_kwargs')
-		if lc_get_pos_kwargs is None:
-			lc_get_pos_kwargs = self.lc_get_pos_defaults
-
-		self.logger.debug(
-			f"getting transient position from lightcurve using args: {lc_get_pos_kwargs}"
-		)
-
 		try:
-			transient_ra, transient_dec = light_curve.get_pos(**lc_get_pos_kwargs)
+			transient_ra = datapoint['body']['ra']
+			transient_dec = datapoint['body']['dec']
 		except IndexError:
 			return T2RunState.MISSING_INFO
 
-		self.logger.debug(
-			"Transient position (ra, dec): {transient_ra:.4f}, {transient_dec:.4f} deg"
-		)
+		if self.debug:
+			self.logger.debug(
+				"Transient position (ra, dec): {transient_ra:.4f}, {transient_dec:.4f} deg"
+			)
 
 		# initialize the catalog quer(ies). Use instance variable to aviod duplicates
 		out_dict: Dict[str, Any] = {}
-		catalogs = run_config.get('catalogs')
-		for catalog, cat_opts in catalogs.items():
-			src, dist = None, None
-			self.logger.debug(f"Loading catalog {catalog} using options: {str(cat_opts)}")
+		for catalog, cat_opts in self.catalogs.items():
 
-			# check options:
-			for opt_key in self.mandatory_keys:
-				if opt_key not in cat_opts.keys():
-					raise KeyError(
-						f"options for catalog {catalog} are missing mandatory "
-						f"{opt_key} argument. Check your run config."
-					)
+			src, dist = None, None
+			if self.debug:
+				self.logger.debug(f"Loading catalog {catalog} using options: {str(cat_opts)}")
 
 			# how do you want to support the catalog?
-			use = cat_opts.get('use')
-			if use == 'extcats':
+			if cat_opts.use == 'extcats':
 
 				# get the catalog query object and do the query
 				catq = self.init_extcats_query(
-					catalog, catq_kwargs=cat_opts.get('catq_kwargs')
+					catalog, catq_kwargs=cat_opts.catq_kwargs
 				)
 
 				src, dist = catq.findclosest(
-					transient_ra, transient_dec, cat_opts['rs_arcsec'],
-					pre_filter=cat_opts.get('pre_filter'),
-					post_filter=cat_opts.get('post_filter')
+					transient_ra, transient_dec, cat_opts.rs_arcsec,
+					pre_filter=cat_opts.pre_filter,
+					post_filter=cat_opts.post_filter
 				)
 
-			elif use == 'catsHTM':
+			elif cat_opts.use == 'catsHTM':
 
 				# catshtm needs coordinates in radians
 				transient_coords = SkyCoord(transient_ra, transient_dec, unit='deg')
 				srcs, colnames, colunits = self.catshtm_client.cone_search(
 					catalog, transient_coords.ra.rad,
-					transient_coords.dec.rad, cat_opts['rs_arcsec']
+					transient_coords.dec.rad, cat_opts.rs_arcsec
 				)
 
 				if len(srcs) > 0:
@@ -243,7 +230,7 @@ class T2CatalogMatch(AbsT2Unit):
 					srcs_tab = Table(asarray(srcs), names=colnames)
 
 					# find out how ra/dec are called in the catalog
-					catq_kwargs = cat_opts.get('catq_kwargs')
+					catq_kwargs = cat_opts.catq_kwargs
 					if catq_kwargs is None:
 						ra_key, dec_key = 'ra', 'dec'
 					else:
@@ -258,19 +245,22 @@ class T2CatalogMatch(AbsT2Unit):
 					)
 			else:
 				raise ValueError(
-					f"use option can not be {use} for catalog {catalog}. valid are 'extcats' or 'catsHTM'"
+					f"use option can not be {cat_opts.use} for catalog {catalog}. "
+					f"valid are 'extcats' or 'catsHTM'"
 				)
 
 			if src is not None:
-				self.logger.debug(f"found counterpart {dist:.2f} arcsec away from transient.")
+				if self.debug:
+					self.logger.debug(f"found counterpart {dist:.2f} arcsec away from transient")
 				# if you found a cp add the required field from the catalog:
 				# if keys_to_append argument is given or if it is equal to 'all'
 				# then take all the columns in the catalog. Otherwise only add the
 				# requested ones.
 				out_dict[catalog] = {'dist2transient': dist}
-				keys_to_append = cat_opts.get('keys_to_append', 'all')
-				if keys_to_append == 'all':
+
+				if not cat_opts.keys_to_append:
 					keys_to_append = src.colnames
+
 				if len(keys_to_append) > 0:
 					to_add = {}
 					for field in keys_to_append:
@@ -283,10 +273,11 @@ class T2CatalogMatch(AbsT2Unit):
 						to_add[field] = val
 					out_dict[catalog].update(to_add)
 			else:
-				self.logger.debug(
-					f"no match found in catalog {catalog} within "
-					f"{cat_opts['rs_arcsec']:.2f} arcsec from transient"
-				)
+				if self.debug:
+					self.logger.debug(
+						f"no match found in catalog {catalog} within "
+						f"{cat_opts.rs_arcsec:.2f} arcsec from transient"
+					)
 				out_dict[catalog] = False
 
 		# return the info as dictionary
