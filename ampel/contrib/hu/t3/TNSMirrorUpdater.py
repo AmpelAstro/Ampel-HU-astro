9a3d6a8b6518123ee6d87f676a9d93d612fac30c
Jakob van Santen <jvansanten@gmail.com>
2020-12-29 00:09:55 +0100

    TNSMirrorUpdater: convert to OpsUnit

--- ampel/contrib/hu/t3/TNSMirrorUpdater.py
+++ ampel/contrib/hu/t3/TNSMirrorUpdater.py
@@ -4,26 +4,25 @@
 # License           : BSD-3-Clause
 # Author            : Jakob van Santen <jakob.van.santen@desy.de>
 # Date              : 13.12.2018
-# Last Modified Date: 13.08.2020
+# Last Modified Date: 29.12.2020
 # Last Modified By  : Jakob van Santen <jakob.van.santen@desy.de>
 
 import asyncio
 import datetime
+from typing import Any, Dict, Optional
 
 from pymongo import MongoClient
 
-from ampel.abstract.AbsT3Unit import AbsT3Unit
+from ampel.abstract.AbsOpsUnit import AbsOpsUnit
 from ampel.contrib.hu.t3.tns.TNSClient import TNSClient
 from ampel.contrib.hu.t3.tns.TNSMirrorDB import TNSMirrorDB
 from ampel.model.Secret import Secret
 
 
-class TNSMirrorUpdater(AbsT3Unit):
+class TNSMirrorUpdater(AbsOpsUnit):
     """
     Sync a local mirror of the TNS database
     """
-
-    require = ("extcats",)
 
     extcats_auth: Secret[dict] = {"key": "extcats/writer"}  # type: ignore[assignment]
     api_key: Secret[str]
@@ -31,14 +30,10 @@ class TNSMirrorUpdater(AbsT3Unit):
     max_parallel_requests: int = 8
     dry_run: bool = False
 
-    def add(self, transients):
-        ...
+    def run(self, beacon: Optional[Dict[str, Any]] = None) -> Optional[Dict[str, Any]]:
 
-    def done(self) -> None:
-        if self.context and "last_run" in self.context:
-            last_run = datetime.datetime.fromtimestamp(self.context["last_run"])
-        else:
-            last_run = datetime.datetime(2020, 12, 25)
+        now = datetime.datetime.utcnow()
+        last_run = beacon["updated"] if beacon else now - datetime.timedelta(days=7)
 
         async def fetch():
             tns = TNSClient(
@@ -49,17 +44,14 @@ class TNSMirrorUpdater(AbsT3Unit):
             )
             return [item async for item in tns.search(public_timestamp=str(last_run))]
 
-        if self.dry_run:
-            new_reports = []
-        else:
+        if not self.dry_run:
             new_reports = asyncio.get_event_loop().run_until_complete(fetch())
+            TNSMirrorDB(
+                MongoClient(
+                    self.context.config.get("resource.extcats", str, raise_exc=True),
+                    **self.extcats_auth.get()
+                ),
+                logger=self.logger,
+            ).add_sources(new_reports)
 
-        assert self.resource
-        db = TNSMirrorDB(
-            MongoClient(self.resource["extcats"], **self.extcats_auth.get()),
-            logger=self.logger,
-        )
-        if self.dry_run:
-            self.logger.warn(str(new_reports))
-        else:
-            db.add_sources(new_reports)
+        return {"updated": now}
