edc7776a29031f9ba52cd3ab40a6fda0008d652f
Jakob van Santen <jvansanten@gmail.com>
2021-09-03 08:50:28 +0200

    TNSTalker: remove legacy test data
    Test data contained TNS API tokens

--- ampel/contrib/hu/test/test_tnstalker.py
+++ ampel/contrib/hu/test/test_tnstalker.py
@@ -1,63 +1,10 @@
-from ampel.contrib.hu.t3.tns.TNSToken import TNSToken
-import json
 from os import environ
-from os.path import dirname, join
 
 import pytest
 
-from ampel.contrib.hu.t3.TNSTalker import TNSTalker, TNSClient, TNS_BASE_URL_SANDBOX
+from ampel.contrib.hu.t3.tns.TNSToken import TNSToken
+from ampel.contrib.hu.t3.TNSTalker import TNSClient, TNS_BASE_URL_SANDBOX
 from ampel.log.AmpelLogger import AmpelLogger
-from ampel.util.legacy.json_v06 import object_hook
-
-
-# Load mixed set of transients for check
-@pytest.fixture
-def t3_TNStvs_mixed():
-    with open(join(dirname(__file__), "t3TransientTalker_testTV.json")) as f:
-        return [json.loads(line, object_hook=object_hook) for line in f]
-
-
-@pytest.fixture
-def t3_TNStvs_good():
-    with open(join(dirname(__file__), "t3TransientTalker_testsubmitTV.json")) as f:
-        return [json.loads(line, object_hook=object_hook) for line in f]
-
-
-@pytest.fixture
-def testrunconfig():
-    return {
-        "logger": AmpelLogger.get_logger(),
-        "submit_tns": False,
-        "sandbox": False,
-        "needed_catalogs": [],
-    }
-
-
-def test_run_t3_TnsTalker_selection(t3_TNStvs_mixed, testrunconfig):
-    """
-    Check whether correct TVs are accepted based on transient information
-    """
-    unit_test = TNSTalker(**testrunconfig)
-    out = [unit_test.accept_tview(tv) for tv in t3_TNStvs_mixed]
-    assert len(out) == 30
-    assert sum(out) == 5
-
-
-@pytest.mark.xfail(reason="tries to connect to TNS")
-def test_run_t3_TnsTalker_tnsnamefound(t3_TNStvs_good, testrunconfig, mocker):
-    """
-    Check whether it find all of these to be submitted. Note - can connect to remove TNS DB!
-    """
-    mock = mocker.patch(
-        "ampel.contrib.hu.t3.TNSTalker.get_tnsname",
-        side_effect=AssertionError("get_tnsname should not be called"),
-    )
-    unit_test = TNSTalker(**testrunconfig)
-    for tv in t3_TNStvs_good:
-        tns_name, tns_internals, jup = unit_test.find_tns_name(tv)
-        assert not mock.called
-        assert tns_name is not None
-
 
 def test_tnsclient():
     if not (api_key := environ.get("TNS_API_KEY")):
