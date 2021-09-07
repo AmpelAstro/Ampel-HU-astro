457b378f7eae684271a8ae2b25e4d0abed30a933
Jakob van Santen <jvansanten@gmail.com>
2021-09-07 14:09:54 +0200

    bump core, ztf

--- setup.py
+++ setup.py
@@ -29,11 +29,11 @@ setup(
     },
     install_requires=[
         'ampel-interface>=0.8.0a0,<0.9',
-        'ampel-core>=0.8.0a6,<0.9',
+        'ampel-core>=0.8.0a8,<0.9',
         'ampel-plots>=0.8.0a0,<0.9',
         'ampel-photometry>=0.8.0a0,<0.9',
         'ampel-alerts>=0.8.0a0,<0.9',
-        'ampel-ztf>=0.8.0a0,<0.9',
+        'ampel-ztf>=0.8.0a4,<0.9',
         "sncosmo",
         "iminuit",
         "sfdmap",
