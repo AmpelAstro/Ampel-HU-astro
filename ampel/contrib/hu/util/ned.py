15ab18e2a3e0da009536685a3a4b5c0bad4d957f
vbrinnel <valery.brinnel@gmail.com>
2021-09-16 13:52:22 +0200

    New module: ampel.contrib.hu.util.ned

---
+++ ampel/contrib/hu/util/ned.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-HU-astro/ampel/contrib/hu/util/ned.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 14.09.2021
+# Last Modified Date: 14.09.2021
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Optional, Tuple
+from ampel.protocol.LoggerProtocol import LoggerProtocol
+
+def check_ned_res(
+	cat_res: dict,
+	logger: LoggerProtocol,
+	spectroscopic: bool = False,
+	z_range: Optional[Tuple[float, float]] = None
+) -> bool:
+
+	if not cat_res.get('z'):
+		logger.info("No redshift found in NED result")
+		return True
+
+	if spectroscopic and cat_res.get('n_spectra', 0) == 0 and cat_res["zflag"] != "SPEC":
+		logger.info("Not a spectroscopic redshift")
+		return True
+
+	if z_range and (cat_res['z'] < z_range[0] or cat_res['z'] > z_range[1]):
+		logger.info("Redshift exceeds allowed values")
+		return True
+
+	return False
