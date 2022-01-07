75e293bdd7bda821859e93d75f078896437e7f4d
Jakob van Santen <jvansanten@gmail.com>
2022-01-07 11:12:06 +0100

    tests: disable get_unit_env

--- ampel/contrib/hu/test/test_config.py
+++ ampel/contrib/hu/test/test_config.py
@@ -2,9 +2,10 @@
 from ampel.config.builder.DistConfigBuilder import DistConfigBuilder
 
 def test_build_config() -> None:
-    cb = DistConfigBuilder(verbose=True, get_env=False)
+    cb = DistConfigBuilder(verbose=True)
     cb.load_distributions()
     config = cb.build_config(
         stop_on_errors=2,
         config_validator="ConfigValidator",
+        get_unit_env=False,
     )
