4de9db3795b9f5117453a6b366fff727f9d3adc3
wombaugh <jnordin@physik.hu-berlin.de>
2022-09-27 09:21:35 +0200

    VOEvent creating T3.

---
+++ ampel/contrib/hu/t2/T2FastDecliner.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File:                ampel/contrib/hu/t2/T2FastDecliner.py
+# License:             BSD-3-Clause
+# Author:              jnordin@physik.hu-berlin.de
+# Date:                28.06.2022
+# Last Modified Date:  26.09.2022
+# Last Modified By:    Jakob Nordin <jnordin@physik.hu-berlin.de>
+
+from astropy.table import Table
+from typing import Any, TYPE_CHECKING
+from ampel.types import UBson
+from ampel.abstract.AbsLightCurveT2Unit import AbsLightCurveT2Unit
+from ampel.struct.UnitResult import UnitResult
+from ampel.view.LightCurve import LightCurve
+
+
+class T2FastDecliner(AbsLightCurveT2Unit):
+    """
+    Determine decline rate in two last obs.
+    Check whether above some limit.
+
+    :param float min_declinerate: Min decline rate for alarm
+    :param float min_trange: Min time diff for consideration (days)
+    :param float max_trange: Max time diff for consideration (days)
+    :param dict[filter_id, filter_name] filter_keys: Filters to check
+
+    :return: dict, decline rate in each filter and
+            bool: fast_decliner
+    """
+
+    min_declinerate: float = 0.1
+    min_trange: float = 0.1
+    max_trange: float = 2.
+    filter_keys: dict[str, str] = {"1": "g", "2": "R"}
+
+    def process(self, light_curve: LightCurve) -> UBson | UnitResult:
+
+        # For each filter, check datapoints for latest datapoints
+        t2_output = {'fast_decliner': False}
+        for filt_id, filt_name in self.filter_keys.items():
+            pps = light_curve.get_ntuples( ["jd", "magpsf", "sigmapsf"],
+                 filters = [
+                    {"attribute": "fid", "operator": "==", "value": int(filt_id)},
+                ]
+            )
+            print(pps)
+            if pps is None or len(pps)<2:
+                continue
+            print(pps)
+            print(pps[0])
+            pps = sorted(pps, key=lambda d: d[0])
+            delta_t = pps[-1][0] - pps[-2][0]
+            delta_mag = pps[-1][1] - pps[-2][1]
+            decline_rate = delta_mag / delta_t
+            t2_output[filt_name] = {'delta_t': delta_t, 'decline_rate': decline_rate}
+            print(delta_t)
+            print(delta_mag)
+            print(decline_rate)
+            if (delta_t>self.min_trange and delta_t<self.max_trange and
+                    decline_rate>self.min_declinerate):
+                t2_output['fast_decliner'] = True
+            print(t2_output)
+
+        return t2_output
---
+++ ampel/contrib/hu/t3/VOEventPublisher.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File:                ampel/contrib/hu/t3/VOEventPublisher.py
+# License:             BSD-3-Clause
+# Author:              jno <jnordin@physik.hu-berlin.de>
+# Date:                26.09.2022
+# Last Modified Date:  26.09.2022
+# Last Modified By:    jno <jnordin@physik.hu-berlin.de>
+
+from itertools import islice
+from typing import Iterable, TYPE_CHECKING, Literal, Any, Optional
+from collections.abc import Generator
+import datetime
+from astropy.time import Time
+
+from ampel.types import T3Send
+from ampel.struct.StockAttributes import StockAttributes
+from ampel.struct.JournalAttributes import JournalAttributes
+from ampel.enum.DocumentCode import DocumentCode
+from ampel.abstract.AbsPhotoT3Unit import AbsPhotoT3Unit
+from ampel.secret.NamedSecret import NamedSecret
+from ampel.view.TransientView import TransientView
+from ampel.view.T2DocView import T2DocView
+from ampel.view.T3Store import T3Store
+from ampel.util.mappings import get_by_path
+
+import voeventparse as vp
+
+
+if TYPE_CHECKING:
+    from ampel.content.JournalRecord import JournalRecord
+
+
+class VOEventPublisher(AbsPhotoT3Unit):
+    """
+
+    Unit for creating a VOEvent pased on T2output and transient LightCurve.
+
+    Selection of fields to save. Matches structure of t2document result dict, e.g.:
+    why_schema = { { 't2_unit_name'  : {
+                'voevent_label' : ['path','to','val'],
+                'voevent_label_2' : ['other','path']
+            },
+        } }
+
+    """
+
+    # Characteristics of the VOEvent
+    voevent_ivorn: str = 'AMPEL/dev'
+    voevent_role: str = 'test'
+    voevent_stream: str = 'myFastDecliners'
+    vovent_streamid: int = 1
+
+    # Selection of fields to output ("WHY?")
+    name_filter: dict[str, str] = {'ZTF name': 'ZTF', 'TNS ID': 'TNS'}
+    # Which datapoints to include in submission?
+    which_photometry: Literal['first','last'] = 'first'    #
+    # Schema for state dependent T2s (one row for each)
+    why_schema: dict[str, Any]
+    # Temporary file name
+    fname = 'voevent.xml'
+
+    def process(self, gen: Generator[TransientView, T3Send, None],
+                t3s: Optional[T3Store] = None) -> None:
+        """
+        Loop through provided TransientViews and extract data according to the
+        configured schema.
+        """
+
+        for k, tran_view in enumerate(gen, 1):
+
+            # Chategorized by stock (internal ampel ID) and channel
+            stock = tran_view.id
+            print(stock)
+            channels = tran_view.stock.get("channel")
+            channel = channels[0] if isinstance(channels, (list, tuple)) and len(channels) == 1 else '/'.join(channels)
+
+            # Get photometry from first|last visit
+            dps = tran_view.get_photopoints()
+            dps = sorted(dps, key=lambda d: d['body']['jd'])
+            if self.which_photometry=='first':
+                dp = dps[0]['body']
+            elif self.which_photometry=='last':
+                dp = dps[-1]['body']
+
+            # Collect t2 information.
+            # Only uses latest state. See TransientTablePublisher for
+            # possible extension to state.
+            t2dict: dict[str, Any] = {}
+            for t2unit, table_entries in self.why_schema.items():
+                for t2res in tran_view.get_t2_views(unit=t2unit):
+                    for label, path in table_entries.items():
+                        if result := get_by_path(t2res.body[-1], path):
+                            t2dict[label] = result
+            if len(t2dict.keys()) == 0:
+                continue
+
+            # Generate VOEvent
+            v = vp.Voevent(stream=self.voevent_stream, stream_id=self.vovent_streamid,
+                       role=self.voevent_role)
+            vp.set_who(v, datetime.datetime.utcnow())
+            vp.set_author(v, title="Results from {} channel".format(channel),
+                      contactName="ampel-info@desy.de")
+            v.Description = "Generated through https://github.com/AmpelProject/Ampel-HU-astro/ampel/contrib/hu/t3/VOEventPublisher.py"
+
+            v.What.append(vp.Param(name="mag", value=dp['magpsf'], ucd="phot.mag"))
+
+            dt = Time(dp['jd'], format='jd').to_datetime()
+            dt = dt.replace(tzinfo=datetime.timezone.utc)
+
+            vp.add_where_when(v,
+                coords=vp.Position2D(ra=dp['ra'], dec=dp['dec'], err=0, units='deg',
+                                                system=vp.definitions.sky_coord_system.utc_fk5_geo),
+                           obs_time=dt,
+                           observatory_location="Palomar P48 / ZTF camera")
+
+            # Add collected results
+            vp.add_how( v, descriptions = [
+                    "{}: {}".format(k,v) for k, v in t2dict.items()
+                ]
+                )
+            vp.add_why(v)
+            v.Why.Description = "Selected based on AMPEL T2s: {}".format(
+                ' '.join(self.why_schema.keys() ) )
+
+            with open(self.fname, 'wb') as f:
+                vp.dump(v, f)
+            with open(self.fname, 'r') as f:
+                for l in f.readlines():
+                    print(l.rstrip())
+
+
+
+
+        return None
