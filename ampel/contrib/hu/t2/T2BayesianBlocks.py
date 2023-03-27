correct calculation of significance of difference between excess blocks and baseline blocks

ampel/contrib/hu/t2/T2BayesianBlocks.py
@@ -210,17 +210,16 @@ class T2BayesianBlocks(AbsLightCurveT2Unit):
                 else:
                     baye_block.loc[idx, "level"] = "excess"
             elif baye_block["level"][idx] != "baseline":
-                if (
-                    baseline + (self.rej_sigma * baseline_sigma)
-                    >= mag
-                    >= baseline - (self.rej_sigma * baseline_sigma)
-                ) or (
-                    baseline + (self.rej_sigma * baseline_sigma)
-                    >= mag - baye_block["mag.err"][idx]
-                ):
+                diff = abs(baseline - mag)
+                diff_e = np.sqrt(baseline_sigma ** 2 + baye_block["mag.err"][idx] ** 2)
+                diff_significance = diff / diff_e
+
+                if diff_significance <= self.rej_sigma:
                     baye_block.loc[idx, "level"] = "baseline"
+
                 else:
                     baye_block.loc[idx, "level"] = "excess"
+
             if "baseline" in baye_block["level"].values:
                 (baseline, baseline_sigma, baseline_rms) = self.calculate_baseline(
                     df, baye_block