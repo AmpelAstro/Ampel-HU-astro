fe305c9bc404ecbfc490a5f6f7fd8c6d3cfe1434
andimatter <hermydrea@web.de>
2023-09-20 16:10:23 +0200

    began implementation of running several Possis models, fixed bug in jobfile template

--- ampel/contrib/hu/t2/T2RunPossis.py
+++ ampel/contrib/hu/t2/T2RunPossis.py
@@ -9,7 +9,7 @@
 
 
 import numpy as np
-import sncosmo # type: ignore[import]
+import sncosmo  # type: ignore[import]
 from sfdmap2.sfdmap import SFDMap  # type: ignore[import]
 from typing import Union
 import copy
@@ -27,6 +27,7 @@ from ampel.view.T2DocView import T2DocView
 from ampel.enum.DocumentCode import DocumentCode
 from ampel.content.T1Document import T1Document
 from ampel.content.DataPoint import DataPoint
+
 
 class T2RunPossis(T2RunSncosmo):
     """
@@ -48,49 +49,81 @@ class T2RunPossis(T2RunSncosmo):
     """
 
     # Parameters determining which POSSIS model will be read
-    possis_base_url: str = 'https://raw.githubusercontent.com/mbulla/kilonova_models/f810e0ec7e7a6ae32624738329e73d561b081372'
-    model_gen: str = 'bns_m3_3comp'
+    possis_base_url: str = "https://raw.githubusercontent.com/mbulla/kilonova_models/f810e0ec7e7a6ae32624738329e73d561b081372"
+    model_gen: str = "bns_m3_3comp"
     mej_dyn: float = 0.01
     mej_wind: float = 0.09
     phi: int = 45
-    cos_theta: float = 0.3    # Typically 0., 0.1, ...1.0
+    cos_theta: float = 0.3  # Typically 0., 0.1, ...1.0
 
-    sncosmo_model_name: str = '_'.join(map(str, [model_gen, mej_dyn,
-        mej_wind, phi, cos_theta]) )
+    sncosmo_model_name: str = "_".join(
+        map(str, [model_gen, mej_dyn, mej_wind, phi, cos_theta])
+    )
+
+    possis_models: dict = {
+        model_gen: {
+            "mej_dyn": mej_dyn,
+            "mej_wind": mej_wind,
+            "phi": phi,
+            "cos_theta": cos_theta,
+        }
+    }
 
     # Fix time to specific explosion timestamp
     # StockTriggerTime assumes the value is updated during runtime
-    explosion_time_jd: None | float | Literal['TriggerTime']
+    explosion_time_jd: None | float | Literal["TriggerTime"]
 
     # Which units should this be changed to
-    t2_dependency: Sequence[StateT2Dependency[Literal[ # type: ignore[assignment]
+    t2_dependency: Sequence[
+        StateT2Dependency[
+            Literal[  # type: ignore[assignment]
                 "T2DigestRedshifts",
                 "T2MatchBTS",
                 "T2PhaseLimit",
                 "T2PropagateStockInfo",
                 "T2HealpixProb",
-            ]]]
+            ]
+        ]
+    ]
 
-    def post_init(self)-> None:
+    def post_init(self) -> None:
         """
         Retrieve POSSIS model.
 
         Note that this could be done once at instance init.
         """
 
+        for model_gen in self.possis_models.keys():
+            self.possis_models[model_gen]["sncosmo_model_name"] = "_".join(
+                map(str, [model_gen, self.possis_models[model_gen]["mej_dyn"], 
+                          self.possis_models[model_gen]["mej_wind"], 
+                          self.possis_models[model_gen]["phi"], 
+                          self.possis_models[model_gen]["cos_theta"]])
+            )
+
+        print("T2RUNPOSSIS::", self.possis_models)
+
+        for model_gen, model_dict in self.possis_models.items():
+            mej_dyn = model_dict["mej_dyn"] if model_dict.get("mej_dyn") else self.mej_dyn
+            mej_wind = model_dict["mej_wind"] if model_dict.get("mej_wind") else self.mej_wind
+            phi = model_dict["phi"] if model_dict.get("phi") else self.phi
+            cos_theta = model_dict["cos_theta"] if model_dict.get("cos_theta") else self.cos_theta
+
+            apply_mwcorrection = model_dict["apply_mwcorrection"] if model_dict.get("apply_mwcorrection") else self.apply_mwcorrection
+
         model_url = urljoin(
             self.possis_base_url + "/",
-            f"{self.model_gen}/nph1.0e+06_mejdyn{self.mej_dyn:05.3f}_mejwind{self.mej_wind:05.3f}_phi{self.phi}.txt"
+            f"{self.model_gen}/nph1.0e+06_mejdyn{self.mej_dyn:05.3f}_mejwind{self.mej_wind:05.3f}_phi{self.phi}.txt",
         )
+
 
         # Find file
         with urlopen(model_url) as fh:
-
             # Read model Parameters from first three lines
             lines = [next(fh).decode() for _ in range(3)]
             nobs = int(lines[0])
             nwave = int(lines[1])
-            line3 = lines[2].split(' ')
+            line3 = lines[2].split(" ")
             ntime = int(line3[0])
             t_i = float(line3[1])
             t_f = float(line3[2])
@@ -100,24 +133,34 @@ class T2RunPossis(T2RunSncosmo):
             # Limit to one angle
             # Note: U. Feindt developed model where angle was fit, left out for know
             theta_mask = np.isclose(self.cos_theta, model_cos_theta)
-            if not sum(theta_mask)==1:
+            if not sum(theta_mask) == 1:
                 raise ValueError("Model cos_theta {model_cos_theta} not defined")
 
             # Read model data
             mdata = np.genfromtxt(fh)
-        wave = mdata[0 : int(nwave), 0] # noqa
-        flux = np.array([mdata[i * int(nwave) : i * int(nwave) + int(nwave), 1:] for i in range(int(nobs))]).T
+        wave = mdata[0 : int(nwave), 0]  # noqa
+        flux = np.array(
+            [
+                mdata[i * int(nwave) : i * int(nwave) + int(nwave), 1:]
+                for i in range(int(nobs))
+            ]
+        ).T
 
         # Reduce to one angle
-        flux_1angle = flux[:,:,theta_mask].squeeze()
+        flux_1angle = flux[:, :, theta_mask].squeeze()
         # Create model
-        source = sncosmo.TimeSeriesSource(phase, wave, flux_1angle, name=self.sncosmo_model_name)
+        source = sncosmo.TimeSeriesSource(
+            phase, wave, flux_1angle, name=self.sncosmo_model_name
+        )
 
         # Setup model, with or without MW correction
         if self.apply_mwcorrection:
             dust = sncosmo.models.CCM89Dust()
             self.sncosmo_model = sncosmo.Model(
-                source=source, effects=[dust], effect_names=["mw"], effect_frames=["obs"]
+                source=source,
+                effects=[dust],
+                effect_names=["mw"],
+                effect_frames=["obs"],
             )
             self.dustmap = SFDMap()
             self.fit_params = copy.deepcopy(self.sncosmo_model.param_names)
@@ -125,7 +168,6 @@ class T2RunPossis(T2RunSncosmo):
         else:
             self.sncosmo_model = sncosmo.Model(source=source)
             self.fit_params = copy.deepcopy(self.sncosmo_model.param_names)
-
 
         # If redshift _should_ be provided we remove this from fit parameters
         if self.redshift_kind is not None or self.backup_z is not None:
@@ -139,11 +181,11 @@ class T2RunPossis(T2RunSncosmo):
 
         self.default_param_vals = self.sncosmo_model.parameters
 
-
-    def process(self,
+    def process(
+        self,
         compound: T1Document,
         datapoints: Sequence[DataPoint],
-        t2_views: Sequence[T2DocView]
+        t2_views: Sequence[T2DocView],
     ) -> Union[UBson, UnitResult]:
         """
         Fit the loaded model to the data provided as a LightCurve.
@@ -155,14 +197,19 @@ class T2RunPossis(T2RunSncosmo):
             for t2_view in t2_views:
                 if not t2_view.unit in ["T2PropagateStockInfo", "T2HealpixProb"]:
                     continue
-                self.logger.debug('Parsing t2 results from {}'.format(t2_view.unit))
-                t2_res = res[-1] if isinstance(res := t2_view.get_payload(), list) else res
-                if not 'trigger_time' in t2_res.keys():
-                    self.logger.info('No explosion time',extra={'t2res':t2_res})
+                self.logger.debug("Parsing t2 results from {}".format(t2_view.unit))
+                t2_res = (
+                    res[-1] if isinstance(res := t2_view.get_payload(), list) else res
+                )
+                if not "trigger_time" in t2_res.keys():
+                    self.logger.info("No explosion time", extra={"t2res": t2_res})
                     return UnitResult(code=DocumentCode.T2_MISSING_INFO)
-                self.explosion_time_jd = float( t2_res['trigger_time'] )
+                self.explosion_time_jd = float(t2_res["trigger_time"])
                 # Reset model
-                self.logger.debug('reset explosion time', extra={'explosion_time': self.explosion_time_jd})
+                self.logger.debug(
+                    "reset explosion time",
+                    extra={"explosion_time": self.explosion_time_jd},
+                )
                 self.sncosmo_model.set(t0=self.explosion_time_jd)
                 self.fit_params.remove("t0")
 
