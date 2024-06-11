9fcfa369d180e03266e492b4c0319d1441b921d4
andimatter <hermydrea@web.de>
2024-06-11 16:21:16 +0200

    template sync, remove debug prints

--- ampel/contrib/hu/t2/T2KilonovaStats.py
+++ ampel/contrib/hu/t2/T2KilonovaStats.py
@@ -9,8 +9,8 @@
 
 import json
 import os
-from collections.abc import Sequence
-from typing import Any, Literal
+from collections.abc import Iterable, Sequence
+from typing import Any, Literal, no_type_check
 
 import numpy as np
 import pandas as pd
@@ -31,14 +31,14 @@ def jsonify(string: str):
     tmp_str = tmp_str.replace("True", "true")
     tmp_str = tmp_str.replace("False", "false")
     tmp_str = tmp_str.replace("inf", "Infinity")
-    return tmp_str  # noqa: RET504
-
+    return tmp_str
 
 def dejsonify(arg):
-    try:
+    #print("got:",arg)
+    try: 
         return float(arg)
     except ValueError:
-        c = {"-Infinity": -np.inf, "Infinity": np.inf, "NaN": np.nan}
+        c = {"-Infinity":-np.inf, "Infinity":np.inf, "NaN":np.nan}
         return c[arg]
 
 
@@ -46,7 +46,7 @@ class T2KilonovaStats(AbsTiedStateT2Unit):
     """
     Evaluate kilonovaness stats for transient given map distance and number of detections.
     """
-
+    
     # Which units should this be chained to
     t2_dependency: Sequence[StateT2Dependency[Literal["T2KilonovaEval"]]]
 
@@ -54,20 +54,22 @@ class T2KilonovaStats(AbsTiedStateT2Unit):
     binned_gaus_dict: dict = {}
 
     def post_init(self):
-        """Retrieve stored gauss parameters."""
+        #print("T2KILONOVASTATS POST INIT")
         with open(
             os.path.join(self.data_dir, "binned_gaus_params.json"), encoding="utf-8"
         ) as file:
             self.binned_gaus_dict = json.load(file, parse_constant=dejsonify)
             file.close()
+        
+        #print(self.binned_gaus_dict)
 
     def get_keys(self, map_dist=np.inf, ndet=np.inf):
-        """Determine which distance and #detections bin event belongs in"""
         ndet_key = np.inf
         map_key = np.inf
         if ndet:
             for key in self.binned_gaus_dict:
                 fkey = dejsonify(key)
+                #print("NDET key:: ", key, " is made ", fkey, " ", type(fkey), " ndet = ", ndet, " ", type(ndet))
                 if ndet <= fkey:
                     ndet_key = key
                     break
@@ -75,25 +77,28 @@ class T2KilonovaStats(AbsTiedStateT2Unit):
         if map_dist:
             for key in self.binned_gaus_dict[ndet_key]:
                 fkey = dejsonify(key)
-                # print(fkey)
+                #print(fkey)
                 if map_dist <= fkey:
                     map_key = key
                     break
 
         return map_key, ndet_key
-
+    
     def gaus_cdf(self, xval, loc: float = 0, scale: float = 1):
         return stats.norm.cdf(xval, loc=loc, scale=scale)
 
     def get_kn_stats(self, map_dist=np.inf, ndet=np.inf):
         map_key, ndet_key = self.get_keys(map_dist=map_dist, ndet=ndet)
+
+        #print(ndet_key, map_key)
         return self.binned_gaus_dict[ndet_key][map_key]
 
     def get_kn_cumprob(self, kilonovaness, map_dist=np.inf, ndet=np.inf):
         gaus_stats = self.get_kn_stats(map_dist=map_dist, ndet=ndet)
-        return self.gaus_cdf(
-            kilonovaness, loc=gaus_stats["loc"], scale=gaus_stats["scale"]
-        )
+        #print("GAUS STATS ", gaus_stats, " ", type(gaus_stats))
+
+        return self.gaus_cdf(kilonovaness, loc=gaus_stats["loc"], scale=gaus_stats["scale"])
+ 
 
     def get_kn_densitiy_stats(self, kilonovaness, map_area, map_dist=np.inf):
         map_key, _ = self.get_keys(map_dist=map_dist, ndet=np.inf)
@@ -103,7 +108,10 @@ class T2KilonovaStats(AbsTiedStateT2Unit):
         file_base = os.path.join(data_dir, "densities")
 
         density_files = os.listdir(file_base)
-        files = [dfile for dfile in density_files if "-" + str(map_key) in dfile]
+        #print()
+        #print("T2KILONVOANESSSTATS:: ", map_key, type(map_key), density_files)
+        #print()
+        files = [dfile for dfile in density_files if "-" + str(map_key) in dfile]  # noqa: RUF015
         if files:
             file = files[0]
         else:
@@ -127,24 +135,30 @@ class T2KilonovaStats(AbsTiedStateT2Unit):
             "dist_range": dist_range,
         }
 
-        # statement = f"For map of area = {map_area} squ.deg. at {dist_range} Mpc, expect {exp_kn:.1f} (+{exp_kn_pls:.1f} -{exp_kn_min:.1f}) candidates with kn = {kilonovaness}."
-        return stats_dict  # noqa: RET504
+        statement = f"For map of area = {map_area} squ.deg. at {dist_range} Mpc, expect {exp_kn:.1f} (+{exp_kn_pls:.1f} -{exp_kn_min:.1f}) candidates with kn = {kilonovaness}."
+        # print(statement)
+        return stats_dict
 
     def get_kn_total_stats(self, kilonovaness, map_area, map_dist=np.inf, ndet=np.inf):
         gaus_stats = (
             1 - self.get_kn_cumprob(kilonovaness, map_dist=map_dist, ndet=ndet)
         ) * 100
+        #print(
+        #    f"Map at this distance with this ndet, {gaus_stats:.5f}% of events have kn >= {kilonovaness}"
+        #)
+
         res = self.get_kn_densitiy_stats(kilonovaness, map_area, map_dist=map_dist)
         res["gaus_percent"] = gaus_stats
         return res
 
     def kilonovaness_statistics(self, t2res: dict[str, Any]) -> None | dict[str, Any]:
+        #print("KEYS:: ", t2res.keys())
         info = self.get_kn_total_stats(
             kilonovaness=t2res["kilonovaness"],
             map_area=t2res["map_area"],
             map_dist=t2res["map_dist"],
         )
-        return info  # noqa: RET504
+        return info
 
     def process(
         self,
@@ -152,6 +166,7 @@ class T2KilonovaStats(AbsTiedStateT2Unit):
         datapoints: Sequence[DataPoint],
         t2_views: Sequence[T2DocView],
     ) -> UBson | UnitResult:
+        #print("T2KILONOVASTATS:: process")
         for t2_view in t2_views:
             self.logger.debug(f"Parsing t2 results from {t2_view.unit}")
             t2_res = res[-1] if isinstance(res := t2_view.get_payload(), list) else res
--- ampel/contrib/hu/util/automation/get_kn.py
+++ ampel/contrib/hu/util/automation/get_kn.py
@@ -18,9 +18,7 @@ from ampel.contrib.hu.util.automation.jobfileWriter import writeJobfilesFromDict
 jobfile = ""
 
 
-EXEC_DIR = (
-    "/mnt/c/Users/Public/Documents/Uni/master/masterarbeit/ampel/ampel-results/remote"
-)
+EXEC_DIR = "./" #  "/mnt/c/Users/Public/Documents/Uni/master/masterarbeit/ampel/ampel-results/O4"
 CONF_FILE = "/mnt/c/Users/Public/Documents/Uni/master/masterarbeit/ampel/Ampel-HU-astro/ampel_conf.yaml"
 VAULT_FILE = "/mnt/c/Users/Public/Documents/Uni/master/masterarbeit/ampel/Ampel-HU-astro/vault.yaml"
 JOBFILE_TEMP = "/mnt/c/Users/Public/Documents/Uni/master/masterarbeit/ampel/Ampel-HU-astro/examples/remote/ligo_automated_template.yml"
@@ -29,64 +27,75 @@ JOBFILE_TEMP = "/mnt/c/Users/Public/Documents/Uni/master/masterarbeit/ampel/Ampe
 ########### get event info
 
 # gw_name = "GW240109a"
-gw_name = sys.argv[1]
-gw_name = gw_name.replace("GW", "")
-gw_name = gw_name.replace("S", "")
-event_name = "S" + gw_name
+gw_names = sys.argv[1:]
+print(type(gw_names))
 
-client = GraceDb()
+if len(gw_names) < 1:
+    gw_names = [gw_names]
 
-try:
-    response = client.files(event_name)
-except ligo.gracedb.exceptions.HTTPError as e:
-    print("GraceDB API request exited with ", e)
-    sys.exit()
+print(gw_names)
+for k, gw_name in enumerate(gw_names):
+#gw_name = sys.argv[1]
+    print("#########################################################")
+    print(f"############### - MAP {gw_name} - {k+1}/{len(gw_names)} - ###############")
+    print("#########################################################")
+    gw_name = gw_name.replace("GW", "")
+    gw_name = gw_name.replace("S", "")
+    event_name = "S" + gw_name
 
-# if response.status_code != 201:
-#    print("GraceDB API request exited with response code ", response.status_code)
-#    sys.exit()
-file_contents = response.json()
+    client = GraceDb()
+
+    try:
+        response = client.files(event_name)
+    except ligo.gracedb.exceptions.HTTPError as e:
+        print("GraceDB API request exited with ", e)
+        sys.exit()
+
+    # if response.status_code != 201:
+    #    print("GraceDB API request exited with response code ", response.status_code)
+    #    sys.exit()
+    file_contents = response.json()
 
 
-fits_files = [key for key in file_contents if ".fits.gz" in key]
+    fits_files = [key for key in file_contents if ".fits.gz" in key]
 
-# print(fits_files)
+    # print(fits_files)
 
-map_url = ""
-map_file = "LALInference.fits.gz"
-if map_file not in fits_files:
-    map_file = "bayestar.fits.gz"
-if map_file not in fits_files:
-    print("No working map file found")
-    raise ValueError(f"No map file found for superevent {event_name}")
-else:
-    map_url = file_contents[map_file]
+    map_url = ""
+    map_file = "LALInference.fits.gz"
+    if map_file not in fits_files:
+        map_file = "bayestar.fits.gz"
+    if map_file not in fits_files:
+        print("No working map file found")
+        raise ValueError(f"No map file found for superevent {event_name}")
+    else:
+        map_url = file_contents[map_file]
 
-print()
-print("+++++++++ \tFOUND EVENT ON GRACEDB \t+++++++++ \n")
-# print(map_url)
+    print()
+    print("+++++++++ \tFOUND EVENT ON GRACEDB \t+++++++++ \n")
+    # print(map_url)
 
-########### write the jobfile
+    ########### write the jobfile
 
-print(f"+++++++++ \t WRITING JOBFILE\t{event_name}\t +++++++++ \n")
+    print(f"+++++++++ \t WRITING JOBFILE\t{event_name}\t +++++++++ \n")
 
-jobfile_list_dict = {
-    gw_name: {
-        "map_url": map_url,
-        # "map_name": map_name,
+    jobfile_list_dict = {
+        gw_name: {
+            "map_url": map_url,
+            # "map_name": map_name,
+        }
     }
-}
 
-jobfile = writeJobfilesFromDict(
-    JOBFILE_TEMP, jobfile_list_dict, commonName="automated", saveDir=EXEC_DIR
-)[0]
-
-
-########### execute the jobfile
-print(f"+++++++++\tEXECUTE JOBFILE \t{jobfile}\t +++++++++ \n")
+    jobfile = writeJobfilesFromDict(
+        JOBFILE_TEMP, jobfile_list_dict, commonName="automated", saveDir=EXEC_DIR
+    )[0]
 
 
-job_call = ""
-job_call += f"ampel job --schema {jobfile} --config {CONF_FILE} --secrets {VAULT_FILE}"
-# print(job_call)
-executeJobfile(job_call, EXEC_DIR, max_retries=2)
+    ########### execute the jobfile
+    print(f"+++++++++\tEXECUTE JOBFILE \t{jobfile}\t +++++++++ \n")
+
+
+    job_call = ""
+    job_call += f"ampel job --schema {jobfile} --config {CONF_FILE} --secrets {VAULT_FILE}"
+    # print(job_call)
+    executeJobfile(job_call, EXEC_DIR, max_retries=2)
