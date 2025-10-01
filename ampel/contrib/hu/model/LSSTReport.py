b749c245eef96fb59073c76af8781ada1d65c561
Jakob van Santen <jvansanten@gmail.com>
2025-10-01 13:59:15 +0200

    Annotate fields with docs

--- ampel/contrib/hu/model/LSSTReport.py
+++ ampel/contrib/hu/model/LSSTReport.py
@@ -1,38 +1,51 @@
 from collections.abc import Sequence
+from typing import Annotated
+
+from pydantic import Field
 
 from ampel.base.AmpelBaseModel import AmpelBaseModel
 
 
 class PhotometricPoint(AmpelBaseModel):
-    time: float
-    flux: float
-    fluxerr: float
-    band: str
-    zp: float
-    zpsys: str
+    """Observed photometric point in an LSST alert report"""
+
+    time: Annotated[float, Field(description="epoch of observation")]
+    flux: Annotated[float, Field(description="observed flux")]
+    fluxerr: Annotated[float, Field(description="flux uncertainty")]
+    band: Annotated[str, Field(description="photometric band")]
+    zp: Annotated[float, Field(description="zero point")]
+    zpsys: Annotated[str, Field(description="zero point system")]
 
 
 class Object(AmpelBaseModel):
-    id: int
-    ra: float
-    ra_err: float
-    dec: float
-    dec_err: float
-    ra_dec_cov: float
+    """Object associated with an LSST alert report (diaObject)"""
+
+    id: Annotated[int, Field(description="diaObjectId")]
+    ra: Annotated[float, Field(description="right ascension (deg)")]
+    ra_err: Annotated[float, Field(description="right ascension uncertainty (deg)")]
+    dec: Annotated[float, Field(description="declination")]
+    dec_err: Annotated[float, Field(description="declination uncertainty")]
+    ra_dec_cov: Annotated[
+        float, Field(description="right ascension/declination covariance")
+    ]
     # redshift: float
     # redshift_err: float
 
 
 class ModelClassification(AmpelBaseModel):
-    model: str
-    probabilities: dict[str, float]
+    model: Annotated[str, Field(description="name of the model")]
+    probabilities: Annotated[
+        dict[str, float], Field(description="probabilities for each class")
+    ]
 
 
 class Classification(AmpelBaseModel):
-    name: str
-    version: str
-    info: str | None = None
-    models: list[ModelClassification]
+    name: Annotated[str, Field(description="classifier name")]
+    version: Annotated[str, Field(description="classifier version")]
+    info: Annotated[str | None, Field(description="additional information about the classifier")]
+    models: Annotated[
+        list[ModelClassification], Field(description="list of model classifications")
+    ]
 
 
 class Host(AmpelBaseModel):
@@ -52,6 +65,10 @@ class Feature(AmpelBaseModel):
 
 
 class LSSTReport(AmpelBaseModel):
+    """
+    Data model for LSST alert reports from Ampel.
+    """
+
     object: Object
     photometry: Sequence[PhotometricPoint]
     classification: list[Classification] = []
