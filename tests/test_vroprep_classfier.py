b18a632a554d98734d1320077b6408d3877a8bd7
Jakob van Santen <jvansanten@gmail.com>
2025-12-18 11:22:53 +0100

    turn up tolerance even more

--- tests/test_vroprep_classfier.py
+++ tests/test_vroprep_classfier.py
@@ -10,7 +10,7 @@ from ampel.cli.JobCommand import JobCommand
 from ampel.model.job.JobModel import JobModel
 
 
-def round_float(value: float, path, rel=1e-6):
+def round_float(value: float, path, rel=1e-4):
     """Round float to avoid snapshot mismatches due to small numerical differences."""
     if np.isfinite(value):
         return round(value / rel) * rel
