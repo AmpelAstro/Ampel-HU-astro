5da4884b7ad7756b03f9a48c853a23f00f24a97b
JannisNe <necker@physik.hu-berlin.de>
2026-03-17 13:39:01 +0100

    bugfixes, add tag

--- ampel/contrib/hu/ingest/ZiArchiveAugmenter.py
+++ ampel/contrib/hu/ingest/ZiArchiveAugmenter.py
@@ -5,8 +5,10 @@ import numpy as np
 from ampel.alert.AmpelAlert import AmpelAlert
 from ampel.content.DataPoint import DataPoint
 from ampel.contrib.hu.ingest.AbsArchiveAugmenter import AbsArchiveAugmenter
-from ampel.ztf.alert.ZiAlertSupplier import ZiAlertSupplier
+from ampel.types import Tag
+from ampel.view.ReadOnlyDict import ReadOnlyDict
 from ampel.ztf.base.ArchiveUnit import ArchiveUnit
+from ampel.ztf.util.ZTFIdMapper import to_ampel_id
 
 
 def mean_position(ra: list[float], dec: list[float]) -> tuple[float, float]:
@@ -31,16 +33,18 @@ class ZiArchiveAugmenter(AbsArchiveAugmenter, ArchiveUnit):
     # the radius in which to look for ZTF counterparts
     radius_arcsec: float = 1
 
+    # tag to add to the alert
+    tag: Tag | None | list[Tag] = None
+
     @staticmethod
     def select_closest_ztf_source(
         alert_dicts: list[dict[str, Any]], ra_ref: float, dec_ref: float
-    ) -> dict[str, Any]:
+    ) -> tuple[list[dict[str, Any]], float]:
         ztf_names = np.unique([ad["objectId"] for ad in alert_dicts])
         sorted_alerts_dict = {
-            zn: [ad["candidate"] for ad in alert_dicts if ad["objectId"] == zn]
-            for zn in ztf_names
+            zn: [ad for ad in alert_dicts if ad["objectId"] == zn] for zn in ztf_names
         }
-        mp = np.inf
+        min_distance = np.inf
         closest_ztf_name = None
         for zn, ads in sorted_alerts_dict.items():
             mp = mean_position(
@@ -52,8 +56,8 @@ class ZiArchiveAugmenter(AbsArchiveAugmenter, ArchiveUnit):
                 * np.cos(np.radians(dec_ref)) ** 2
                 + (np.radians(mp[1]) - np.radians(dec_ref)) ** 2
             )
-            if distance < mp:
-                mp = distance
+            if distance < min_distance:
+                min_distance = distance
                 closest_ztf_name = zn
 
         if closest_ztf_name is None:
@@ -61,14 +65,9 @@ class ZiArchiveAugmenter(AbsArchiveAugmenter, ArchiveUnit):
             raise ValueError("No ZTF source found!")
 
         # selecting the alerts for the closest ZTF source
-        selected_alerts = sorted(
+        return sorted(
             sorted_alerts_dict[closest_ztf_name], key=lambda x: x["candidate"]["jd"]
-        )
-
-        # mimicking the structure of a photopoints query result, so we can use the ZiAlertSupplier later
-        pps_dict = selected_alerts[-1]
-        pps_dict["prv_candidates"] = [ad for ad in selected_alerts[:-1]]
-        return pps_dict
+        ), min_distance
 
     def get_alerts(
         self, dps: list[DataPoint], jd_center: float, time_pre: float, time_post: float
@@ -80,12 +79,27 @@ class ZiArchiveAugmenter(AbsArchiveAugmenter, ArchiveUnit):
             [dp["body"]["ra"] for dp in detections],
             [dp["body"]["dec"] for dp in detections],
         )
-        url = f"/alerts/cone_search?ra={ra}&dec={dec}&radius={self.radius_arcsec / 3600}&jd_start={jd_center - time_pre}&jd_end={jd_center + time_post}"
+        url = (
+            f"/alerts/cone_search?"
+            f"ra={ra}&dec={dec}&radius={self.radius_arcsec / 3600}&"
+            f"jd_start={jd_center - time_pre}&jd_end={jd_center + time_post}"
+        )
         queried_alerts = self.session.get(url)
         queried_detections = [
-            a for a in queried_alerts if "ra" in a
+            a for a in queried_alerts if "ra" in a["candidate"]
         ]  # TODO: refine detection definition
         if len(queried_detections) == 0:
             return None
-        closest_ztf_alerts = self.select_closest_ztf_source(queried_detections, ra, dec)
-        return ZiAlertSupplier.shape_alert_dict(closest_ztf_alerts)
+        ztf_alerts_from_closest_object, distance_to_closest_ztf_source = (
+            self.select_closest_ztf_source(queried_detections, ra, dec)
+        )
+
+        latest_alert_from_closest_source = ztf_alerts_from_closest_object[-1]
+
+        return AmpelAlert(
+            id=latest_alert_from_closest_source["candid"],
+            stock=to_ampel_id(latest_alert_from_closest_source["objectId"]),
+            datapoints=tuple(d["candidate"] for d in ztf_alerts_from_closest_object),
+            extra=ReadOnlyDict({"name": latest_alert_from_closest_source["objectId"]}),
+            tag=self.tag,
+        )
