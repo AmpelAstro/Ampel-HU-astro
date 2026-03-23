440b913d72ff926e046daa820c802fbdfcbeeb3c
JannisNe <necker@physik.hu-berlin.de>
2026-03-23 17:27:40 +0100

    rename to adder

--- ampel/contrib/hu/t0/LSSTMongoMuxerAdder.py
+++ ampel/contrib/hu/t0/LSSTMongoMuxerAdder.py
@@ -1,25 +1,25 @@
 from ampel.content.DataPoint import DataPoint
-from ampel.contrib.hu.augment.AbsAugmenter import AbsAugmenter
+from ampel.contrib.hu.t0.AbsAdder import AbsAdder
 from ampel.lsst.ingest.LSSTMongoMuxer import LSSTMongoMuxer
 from ampel.model.UnitModel import UnitModel
 from ampel.types import StockId
 
 
-class LSSTMongoMuxerAugmenter(LSSTMongoMuxer):
-    augment: UnitModel
+class LSSTMongoMuxerAdder(LSSTMongoMuxer):
+    add: UnitModel
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        self._augment = self.context.loader.new_context_unit(
-            model=self.augment,
+        self._adder = self.context.loader.new_context_unit(
+            model=self.add,
             context=self.context,
             logger=self.logger,
-            sub_type=AbsAugmenter,
+            sub_type=AbsAdder,
         )
 
     def process(
         self, dps_al: list[DataPoint], stock_id: None | StockId = None
     ) -> tuple[None | list[DataPoint], None | list[DataPoint]]:
         dps_insert, dps_combine = super().process(dps_al, stock_id)
-        aug_insert, aug_combine = self._augment.process(dps_combine, stock_id)
+        aug_insert, aug_combine = self._adder.process(dps_combine, stock_id)
         return dps_insert + aug_insert, dps_combine + aug_combine
