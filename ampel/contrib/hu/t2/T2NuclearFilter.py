d9b06d05c5e915f3b5a9ae4f24e2a9761cc19096
JannisNe <necker@physik.hu-berlin.de>
2026-04-29 17:16:08 +0200

    util to get unit mapping for catalog match result

--- ampel/contrib/hu/t2/T2NuclearFilter.py
+++ ampel/contrib/hu/t2/T2NuclearFilter.py
@@ -14,6 +14,9 @@ from astropy.coordinates.angles import angular_separation
 
 from ampel.abstract.AbsTiedPointT2Unit import AbsTiedPointT2Unit
 from ampel.content.DataPoint import DataPoint
+from ampel.contrib.hu.util.catalog_match_position_units import (
+    get_catalog_position_unit_map,
+)
 from ampel.struct.UnitResult import UnitResult
 from ampel.types import UBson
 from ampel.view.T2DocView import T2DocView
@@ -22,6 +25,18 @@ from ampel.view.T2DocView import T2DocView
 class T2NuclearFilter(AbsTiedPointT2Unit):
     match_dist_arcsec: float
     group_matches_within_arcsec: float = 0.5
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # set up unit mapping for positions
+        unit_mapping = get_catalog_position_unit_map()
+        convert_to_rad = []
+        for name, units in unit_mapping.items():
+            if ((u := units["ra"]) is None) or (u.lower().startswith("deg")):
+                convert_to_rad.append(name)
+        self._known_mapping = list(unit_mapping.values())
+        self._convert_to_rad = convert_to_rad
 
     def process(
         self, datapoint: DataPoint, t2_views: Sequence[T2DocView]
@@ -51,21 +66,60 @@ class T2NuclearFilter(AbsTiedPointT2Unit):
         if not matches:
             return res
 
+        # normalize keys
+        matches = {
+            k: {kk.lower(): vv for kk, vv in v.items()} for k, v in matches.items()
+        }
+
+        # PS1_photoz is special
+        if "PS1_photoz" in matches:
+            matches["PS1_photoz"]["ra"] = matches["PS1_photoz"]["ramean"]
+            matches["PS1_photoz"]["dec"] = matches["PS1_photoz"]["decmean"]
+
+        # convert to radians where necessary
+        for name in matches:
+            for k in ["ra", "dec"]:
+                if name in self._convert_to_rad:
+                    matches[name][k] = np.radians(float(matches[name][k]))
+                else:
+                    matches[name][k] = float(matches[name][k])
+
         # find the closest match
-        match_map = np.array(
-            [(k, v["dist2transient"], v["ra"], v["dec"]) for k, v in matches.items()]
-        )
-        best_match_id = np.argmin(match_map[:, 1])
-        dist = matches[best_match_id, 1]
+        try:
+            match_map = np.array(
+                [
+                    (k, float(v["dist2transient"]), float(v["ra"]), float(v["dec"]))
+                    for k, v in matches.items()
+                ],
+                dtype=[
+                    ("name", "<U30"),
+                    ("distance", "<f8"),
+                    ("ra_rad", "<f8"),
+                    ("dec_rad", "<f8"),
+                ],
+            )
+
+        except KeyError as e:
+            raise e
+
+        best_match_id = np.argmin(match_map["distance"])
+        dist = match_map["distance"][best_match_id]
 
         # find matches that are probably the same object
-        separations = angular_separation(
-            np.radians(match_map[best_match_id, 2]),
-            np.radians(match_map[best_match_id, 3]),
-            np.radians(match_map[:, 2]),
-            np.radians(match_map[:, 3]),
+        separations = (
+            np.degrees(
+                angular_separation(
+                    match_map["ra_rad"][best_match_id],
+                    match_map["dec_rad"][best_match_id],
+                    match_map["ra_rad"],
+                    match_map["dec_rad"],
+                )
+            )
+            * 3600
         )
-        matched_catalogs = match_map[separations < self.group_matches_within_arcsec, 0]
+        matched_catalogs = match_map["name"][
+            separations < np.radians(self.group_matches_within_arcsec / 3600)
+        ]
         res["closest_matches"] = matched_catalogs.tolist()
         res["pass"] = dist <= md
         res["dist"] = dist
---
+++ ampel/contrib/hu/util/catalog_match_position_units.py
@@ -0,0 +1,47 @@
+import json
+from pathlib import Path
+
+
+def get_catalog_position_unit_map() -> dict:
+    """
+    The json file can be produced with
+
+    curl -s "https://ampel.zeuthen.desy.de/api/catalogmatch/catalogs/" \
+      -H "accept: application/json" \
+    | jq '
+    [
+      .[]
+      | {
+          name,
+          ra: [.columns[] | select(
+            (.name | ascii_downcase == "ra") or
+            (.name | ascii_downcase == "ramean") or
+            (.name | ascii_downcase == "raj2000") or
+            (.name | ascii_downcase == "radeg") or
+            (.name | ascii_downcase == "ra_central")
+          )],
+          dec: [.columns[] | select(
+            (.name | ascii_downcase == "dec") or
+            (.name | ascii_downcase == "decmean") or
+            (.name | ascii_downcase == "dej2000") or
+            (.name | ascii_downcase == "dedeg") or
+            (.name | ascii_downcase == "dec_central")
+          )]
+        }
+    ]' > catalog_position_columns.json
+    """
+    filename = Path(__file__).parent / "catalog_position_columns.json"
+    with filename.open() as f:
+        mapping = json.load(f)
+
+    # exclude catalogs with multiple or no columns
+    mapping = {
+        v["name"]: {"ra": v["ra"][0], "dec": v["dec"][0]}
+        for v in mapping
+        if len(v["ra"]) == 1 and len(v["dec"]) == 1
+    }
+
+    # check sanity
+    assert all([v["ra"]["unit"] == v["dec"]["unit"] for v in mapping.values()])
+
+    return mapping
